6569ff3b336969c5612b7d47931bcbae63fae03e
LeoPFreitas <leonardo.cadu2@gmail.com>
2021-01-25 21:32:43 -0300

    Configure dependency injection at base main

---
+++ src/main/java/org/example/application/main/Main.java
@@ -0,0 +1,34 @@
+package org.example.application.main;
+
+import org.example.application.repository.DatabaseBuilder;
+import org.example.application.repository.SqlitePersonDAO;
+import org.example.application.view.WindowLoader;
+import org.example.domain.usecases.person.*;
+
+public class Main {
+
+    public static CreatePersonUseCase createPersonUseCase;
+    public static UpdatePersonUseCase updatePersonUseCase;
+    public static FindPersonUseCase findPersonUseCase;
+    public static RemovePersonUseCase removePersonUseCase;
+
+
+    public static void main(String[] args) {
+        configureInjection();
+        setupDatabase();
+        WindowLoader.main(args);
+    }
+
+    private static void setupDatabase() {
+        DatabaseBuilder dbBuilder = new DatabaseBuilder();
+        dbBuilder.buildDatabaseIfMissing();
+    }
+
+    private static void configureInjection() {
+        PersonDAO personDAO = new SqlitePersonDAO();
+        createPersonUseCase = new CreatePersonUseCase(personDAO);
+        updatePersonUseCase = new UpdatePersonUseCase(personDAO);
+        findPersonUseCase = new FindPersonUseCase(personDAO);
+        removePersonUseCase = new RemovePersonUseCase(personDAO);
+    }
+}
