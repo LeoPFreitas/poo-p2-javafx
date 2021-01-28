e6200cb725f7426d0620c58cf6802191b8ac7f23
LeoPFreitas <leonardo.cadu2@gmail.com>
2021-01-27 22:40:13 -0300

    Add ProductImportDAO interface

---
+++ src/main/java/org/example/domain/usecases/importation/ProductImportDAO.java
@@ -0,0 +1,7 @@
+package org.example.domain.usecases.importation;
+
+import org.example.domain.entities.importation.ImportedProduct;
+import org.example.domain.utils.DAO;
+
+public interface ProductImportDAO extends DAO<ImportedProduct, Long> {
+}
