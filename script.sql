529c3cdf914c239547988d794c5d244c2b892682
LeoPFreitas <leonardo.cadu2@gmail.com>
2021-01-30 12:22:18 -0300

    Change bootstrap data script

--- script.sql
+++ script.sql
@@ -3,23 +3,23 @@ VALUES ('Leonardo', 'Freitas', 'a@asasa.com', null, 4234334, 434334);
 INSERT INTO person(first_name, last_name, email, cnpj, cpf, rg)
 VALUES ('Leonardo sdsd', 'Freitas sdsds', 'sa@dsa.com', null, 324234334, 32434334);
 INSERT INTO person(first_name, last_name, email, cnpj, cpf, rg)
-VALUES ('Leonadsdsrdo', 'Freita dsdss', 'a@ads.com', null, 45654, 43656);
+VALUES ('Gustavo', 'Freita dsdss', 'a@ads.com', null, 45654, 43656);
 INSERT INTO person(first_name, last_name, email, cnpj, cpf, rg)
 VALUES ('Leonardo', 'Freitas', 'asssd@a.com', null, 354334, 4365634);
 INSERT INTO person(first_name, last_name, email, cnpj, cpf, rg)
-VALUES ('Leonardo', 'Freitas', 'dsd2a@a.com', null, 4253434, 434345);
+VALUES ('Pietra', 'Freitas', 'dsd2a@a.com', null, 4253434, 434345);
 INSERT INTO person(first_name, last_name, email, cnpj, cpf, rg)
-VALUES ('Leonardo', 'Freitas', 'asssd@a4343.com', 345343, null, null);
+VALUES ('Pedro', 'Freitas', 'asssd@a4343.com', 345343, null, null);
 INSERT INTO person(first_name, last_name, email, cnpj, cpf, rg)
-VALUES ('Leonardo', 'Freitas', 'dsd2a@a23.com', 4434343434, null, null);
+VALUES ('Joana', 'Freitas', 'dsd2a@a23.com', 4434343434, null, null);
 
 INSERT INTO importation(user, product_name, product_price, product_wight, date, category)
 VALUES (1, 'teste', 2132.3222, 3223, '2020-03-04', 'Vestimentas');
 INSERT INTO importation(user, product_name, product_price, product_wight, date, category)
-VALUES (1, 'teste', 2132.3222, 3223, '2020-03-04', 'Vestimentas');
+VALUES (1, 'teste', 2132.3222, 3223, '2019-03-04', 'Vestimentas');
 INSERT INTO importation(user, product_name, product_price, product_wight, date, category)
-VALUES (1, 'teste', 2132.3222, 3223, '2020-03-04', 'Vestimentas');
+VALUES (1, 'teste', 2132.3222, 3223, '2020-07-14', 'Vestimentas');
 INSERT INTO importation(user, product_name, product_price, product_wight, date, category)
-VALUES (2, 'teste', 2132.3222, 3223, '2020-03-04', 'Vestimentas');
+VALUES (2, 'teste', 2132.3222, 3223, '2020-05-04', 'Vestimentas');
 INSERT INTO importation(user, product_name, product_price, product_wight, date, category)
-VALUES (3, 'teste', 2132.3222, 3223, '2020-03-04', 'Vestimentas');
+VALUES (3, 'teste', 2132.3222, 3223, '2020-03-10', 'Vestimentas');
